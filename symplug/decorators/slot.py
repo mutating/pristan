81fc229589430f023d45e45c46573f59974cd07c
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2026-02-05 13:02:52 +0300

    Rename Papameters to SlotPapameters and update Slot type definitions to support plugin integration

--- symplug/decorators/slot.py
+++ symplug/decorators/slot.py
@@ -1,34 +1,44 @@
-from functools import partial, wraps
-from typing import Callable, Optional, ParamSpec, TypeVar, Union, overload
 from dataclasses import dataclass
+from functools import partial, wraps
+from typing import Callable, List, Optional, ParamSpec, TypeVar, Union, overload
 
-Papameters = ParamSpec('Papameters')
-SlotResult = TypeVar('SlotResult')
-Function = Callable[Papameters, SlotResult]
+SlotPapameters = ParamSpec('Papameters')
+PluginResult = TypeVar('PluginResult')
+SlotResult = Optional[List[PluginResult]]
+SlotFunction = Callable[SlotPapameters, SlotResult]
+PluginFunction = Callable[SlotPapameters, PluginResult]
 
 @dataclass
 class AddictionalArguments:
     how_many: Optional[Union[str, int]]
 
-
 class Slot:
-    def __init__(self, function: Function, arguments: AddictionalArguments) -> None:
-        self.function = function
+    def __init__(self, slot_function: SlotFunction, arguments: AddictionalArguments) -> None:
+        self.slot_function = slot_function
         self.arguments = arguments
+        self.plugins = []
 
-    def __call__(self, *args: Papameters.args, **kwargs: Papameters.args) -> SlotResult:
-        return self.function(*args, **kwargs)
+    def __call__(self, *args: SlotPapameters.args, **kwargs: SlotPapameters.args) -> SlotResult:
+        return self.slot_function(*args, **kwargs)
 
+    def plugin(self, plugin_name: str) -> Callable[[PluginFunction], PluginFunction]:
+        def decorator(plugin_function: PluginFunction) -> PluginFunction:
+            self._compare_signatures(self.slot_function, plugin_function)
+            self.plugins.append(plugin_function)
+            return plugin_function
+        return decorator
 
+    def _compare_signatures(self, slot_function: SlotFunction, plugin_function: PluginFunction) -> None:
+        pass
 
 
 @overload
-def slot(func: Function, /) -> Function: ...
+def slot(func: SlotFunction, /) -> SlotFunction: ...
 
 @overload
-def slot(*, a: str, b: str) -> Callable[[Function], Function]: ...
+def slot(*, a: str, b: str) -> Callable[[SlotFunction], SlotFunction]: ...
 
-def slot(function: Optional[Function] = None, /, *, how_many: Optional[Union[str, int]] = None) -> Union[Function, Callable[[Function], Function]]:
+def slot(function: Optional[SlotFunction] = None, /, *, how_many: Optional[Union[str, int]] = None) -> Union[SlotFunction, Callable[[SlotFunction], SlotFunction]]:
     if function is not None:
         arguments = AddictionalArguments(
             how_many=how_many,
