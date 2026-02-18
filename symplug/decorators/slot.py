111d4df3d86048f0081fedf5dd86ae403ecd7854
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2026-02-18 17:02:57 +0300

    Add signature parameter to Slot and slot decorator for signature matching

--- symplug/decorators/slot.py
+++ symplug/decorators/slot.py
@@ -2,6 +2,9 @@ from dataclasses import dataclass
 from functools import partial, wraps
 from typing import Callable, List, Optional, ParamSpec, TypeVar, Union, overload
 
+from sigmatch import PossibleCallMatcher
+
+
 SlotPapameters = ParamSpec('Papameters')
 PluginResult = TypeVar('PluginResult')
 SlotResult = Optional[List[PluginResult]]
@@ -13,9 +16,10 @@ class AddictionalArguments:
     how_many: Optional[Union[str, int]]
 
 class Slot:
-    def __init__(self, slot_function: SlotFunction, arguments: AddictionalArguments) -> None:
+    def __init__(self, slot_function: SlotFunction, arguments: AddictionalArguments, signature: Optional[str]) -> None:
         self.slot_function = slot_function
         self.arguments = arguments
+        self.signature = signature
         self.plugins = []
 
     def __call__(self, *args: SlotPapameters.args, **kwargs: SlotPapameters.args) -> SlotResult:
@@ -29,7 +33,11 @@ class Slot:
         return decorator
 
     def _compare_signatures(self, slot_function: SlotFunction, plugin_function: PluginFunction) -> None:
-        pass
+        if self.signature is not None:
+            if not PossibleCallMatcher(self.signature).match(plugin_function):
+                raise ValueError()
+        elif not PossibleCallMatcher.from_callable(slot_function) & PossibleCallMatcher.from_callable(plugin_function):
+            raise ValueError()
 
 
 @overload
@@ -38,12 +46,12 @@ def slot(func: SlotFunction, /) -> SlotFunction: ...
 @overload
 def slot(*, a: str, b: str) -> Callable[[SlotFunction], SlotFunction]: ...
 
-def slot(function: Optional[SlotFunction] = None, /, *, how_many: Optional[Union[str, int]] = None) -> Union[SlotFunction, Callable[[SlotFunction], SlotFunction]]:
+def slot(function: Optional[SlotFunction] = None, /, *, how_many: Optional[Union[str, int]] = None, signature: Optional[str] = None) -> Union[SlotFunction, Callable[[SlotFunction], SlotFunction]]:
     if function is not None:
         arguments = AddictionalArguments(
             how_many=how_many,
         )
-        decorator = wraps(function)(Slot(function, arguments))
+        decorator = wraps(function)(Slot(function, arguments, signature))
         return decorator
 
     return partial(slot, how_many=how_many)
