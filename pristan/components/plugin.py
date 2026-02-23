d008b6dde860201bb288b64d8e86c72b6a398dea
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2026-02-23 21:02:58 +0300

    Fix type error message to show expected type name

--- pristan/components/plugin.py
+++ pristan/components/plugin.py
@@ -19,7 +19,7 @@ class Plugin:
         result = self.plugin_function(*args, **kwargs)
 
         if self.type_check and self.expected_result_type is not return_type_sentinel and not check(result, self.expected_result_type):
-            raise TypeError(f'The type {type(result).__name__} of the plugin\'s "{self.name}" return value {repr(result)} does not match the expected type {self.expected_result_type}.')
+            raise TypeError(f'The type {type(result).__name__} of the plugin\'s "{self.name}" return value {repr(result)} does not match the expected type {self.expected_result_type.__name__}.')
 
         return result
 
