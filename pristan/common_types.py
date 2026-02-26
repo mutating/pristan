6cd264f4ffd18b5b774bc7566f8a390d7644a154
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2026-02-27 01:50:11 +0300

    Fix SlotFunction return type to match SlotResult

--- pristan/common_types.py
+++ pristan/common_types.py
@@ -3,5 +3,5 @@ from typing import Callable, List, Optional, ParamSpec, TypeVar
 SlotPapameters = ParamSpec('SlotPapameters')
 PluginResult = TypeVar('PluginResult')
 SlotResult = Optional[List[PluginResult]]
-SlotFunction = Callable[SlotPapameters, SlotResult]
+SlotFunction = Callable[SlotPapameters, Optional[List[PluginResult]]]
 PluginFunction = Callable[SlotPapameters, PluginResult]
