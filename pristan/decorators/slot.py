6e30deb50cd592829970163be84d84b086d8bd64
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2026-02-27 01:23:46 +0300

    Add "pragma: no branch" comments to slot overloads

--- pristan/decorators/slot.py
+++ pristan/decorators/slot.py
@@ -6,10 +6,10 @@ from pristan.components.slot import Slot
 
 
 @overload
-def slot(func: SlotFunction, /) -> SlotFunction: ...
+def slot(func: SlotFunction, /) -> SlotFunction: ...  # pragma: no branch
 
 @overload
-def slot(*, a: str, b: str) -> Callable[[SlotFunction], SlotFunction]: ...
+def slot(*, a: str, b: str) -> Callable[[SlotFunction], SlotFunction]: ...  # pragma: no branch
 
 def slot(function: Optional[SlotFunction] = None, /, *, signature: Optional[str] = None, name: Optional[str] = None, max_plugins: Optional[int] = None, type_check: bool = True) -> Union[SlotFunction, Callable[[SlotFunction], SlotFunction]]:
     if function is not None:
