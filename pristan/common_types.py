7b1a28ab277d661ae27cdaee764f0c988a6edeb3
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2026-02-27 04:21:42 +0300

    Fix type ignore annotation for ParamSpec import

--- pristan/common_types.py
+++ pristan/common_types.py
@@ -3,7 +3,7 @@ from typing import Callable, Dict, List, Optional, TypeVar, Union
 try:
     from typing import ParamSpec
 except ImportError:  # pragma: no cover
-    from typing_extensions import ParamSpec  # typing: ignore[assignment]
+    from typing_extensions import ParamSpec  # type: ignore[assignment]
 
 SlotPapameters = ParamSpec('SlotPapameters')
 PluginResult = TypeVar('PluginResult')
