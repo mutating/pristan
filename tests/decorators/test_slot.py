fbe0163ee524a3d1252927d58d9510a0f813e1f6
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2026-02-27 19:55:06 +0300

    Remove Python version check from test function name

--- tests/decorators/test_slot.py
+++ tests/decorators/test_slot.py
@@ -601,7 +601,7 @@ def test_run_not_empty_default_function_without_plugins_with_not_empty_dict_anno
 
 
 @pytest.mark.skipif(version_info >= (3, 10), reason='On new versions of Python, there is an another mechanism of printing type annotations.')
-def test_run_not_empty_default_function_without_plugins_with_not_empty_dict_annotation_with_wrong_return_type_new_pythons(folder, subscribable_dict_type):
+def test_run_not_empty_default_function_without_plugins_with_not_empty_dict_annotation_with_wrong_return_type(folder, subscribable_dict_type):
     bread_crumbs = []
 
     @folder(slot)
