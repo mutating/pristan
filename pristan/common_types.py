88e2b09dfaef1cd43bd454c486d3d4919120b61a
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2026-02-27 20:18:41 +0300

    Fix type ignore to include unused-ignore

--- pristan/common_types.py
+++ pristan/common_types.py
@@ -9,5 +9,5 @@ except ImportError:  # pragma: no cover
 SlotPapameters = ParamSpec('SlotPapameters')
 PluginResult = TypeVar('PluginResult')
 SlotResult = Optional[Union[List[PluginResult], Dict[str, PluginResult]]]
-SlotFunction = Callable[SlotPapameters, Optional[Union[List[PluginResult], Dict[str, PluginResult]]]]  # type: ignore[valid-type]
+SlotFunction = Callable[SlotPapameters, Optional[Union[List[PluginResult], Dict[str, PluginResult]]]]  # type: ignore[valid-type, unused-ignore]
 PluginFunction = Callable[SlotPapameters, PluginResult]
