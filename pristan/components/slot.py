f64e106a39330cbf6dece5a75037ef68aea25e6d
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2026-03-06 00:11:54 +0300

    Fix type ignore annotation placement in slot.py

--- pristan/components/slot.py
+++ pristan/components/slot.py
@@ -3,7 +3,7 @@ try:
         entry_points,  # type: ignore[import-not-found, unused-ignore]
     )
 except ImportError:  # type: ignore[assignment, unused-ignore] # pragma: no cover
-    from importlib.metadata import (
+    from importlib.metadata import (  # type: ignore[assignment, unused-ignore]
         entry_points,  # type: ignore[assignment, unused-ignore]
     )
 
