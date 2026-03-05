4c1ee5257126ad5699bcddd97435439fcd4e0644
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2026-03-05 23:32:44 +0300

    Mypy's issues

--- tests/smokes/demo/simple_plugins.py
+++ tests/smokes/demo/simple_plugins.py
@@ -1,6 +1,6 @@
 from tests.smokes.demo.simple_slots import simple_slot_1
 
 
-@simple_slot_1.plugin('name')
+@simple_slot_1.plugin('name')  # type: ignore[attr-defined]
 def plugin_1():
     return 1
