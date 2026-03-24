634a5f9e62bf31f48f1cff93f46f2cfdab232526
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2026-03-25 00:58:03 +0300

    Refactor entry point tests to use monkeypatched EntryPoint

--- tests/smokes/test_entry_points.py
+++ tests/smokes/test_entry_points.py
@@ -1,3 +1,7 @@
+from importlib.metadata import EntryPoint
+
+import pristan.components.slot as slot_module
+
 from tests.smokes.demo.simple_slots import (
     simple_slot_1,
     simple_slot_2,
@@ -7,21 +11,36 @@ from tests.smokes.demo.simple_slots import (
 )
 
 
-def test_run_simple_slot():
+def test_run_simple_slot(monkeypatch):
+    def get_entries(group=None):
+        return [EntryPoint(name='name', value='tests.smokes.demo.simple_plugins', group='pristan')]
+
+    monkeypatch.setattr(slot_module, "entry_points", get_entries)
+
     assert not simple_slot_1.loaded
     assert simple_slot_1() == {'name': 1}
     assert simple_slot_1.loaded
     assert simple_slot_1() == {'name': 1}
 
 
-def test_run_simple_slot_with_another_name():
+def test_run_simple_slot_with_another_name(monkeypatch):
+    def get_entries(group=None):
+        return [EntryPoint(name='name2', value='tests.smokes.demo.plugins_another_name', group='another_name')]
+
+    monkeypatch.setattr(slot_module, "entry_points", get_entries)
+
     assert not simple_slot_2.loaded
     assert simple_slot_2() == {'name2': 2}
     assert simple_slot_2.loaded
     assert simple_slot_2() == {'name2': 2}
 
 
-def test_plugins_are_loaded_when_called():
+def test_plugins_are_loaded_when_called(monkeypatch):
+    def get_entries(group=None):
+        return [EntryPoint(name='name', value='tests.smokes.demo.simple_plugins', group='pristan')]
+
+    monkeypatch.setattr(slot_module, "entry_points", get_entries)
+
     assert not simple_slot_3.loaded
 
     assert simple_slot_3() == {'name': 1}
@@ -29,7 +48,12 @@ def test_plugins_are_loaded_when_called():
     assert simple_slot_3.loaded
 
 
-def test_plugins_are_loaded_when_keys_readed():
+def test_plugins_are_loaded_when_keys_readed(monkeypatch):
+    def get_entries(group=None):
+        return [EntryPoint(name='name', value='tests.smokes.demo.simple_plugins', group='pristan')]
+
+    monkeypatch.setattr(slot_module, "entry_points", get_entries)
+
     assert not simple_slot_4.loaded
 
     assert simple_slot_4.keys() == ('name',)
@@ -37,7 +61,12 @@ def test_plugins_are_loaded_when_keys_readed():
     assert simple_slot_4.loaded
 
 
-def test_plugins_are_loaded_when_getitem():
+def test_plugins_are_loaded_when_getitem(monkeypatch):
+    def get_entries(group=None):
+        return [EntryPoint(name='name', value='tests.smokes.demo.simple_plugins', group='pristan')]
+
+    monkeypatch.setattr(slot_module, "entry_points", get_entries)
+
     assert not simple_slot_5.loaded
 
     assert len(simple_slot_5['name']) == 1
