cbbda5851f5eac4a03a395ade37ea268cbf859fb
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2026-03-30 17:03:32 +0300

    Remove transfunctions, use transtests instead

--- tests/units/conftest.py
+++ tests/units/conftest.py
@@ -2,19 +2,6 @@ from sys import version_info
 from typing import Dict, List
 
 import pytest
-from transfunctions import transfunction
-
-
-@pytest.fixture(params=['async', 'sync', 'generator'])
-def transformed(request):
-    def transformator_function(function):
-        if request.param == 'sync':
-            return function
-        if request.param == 'async':
-            return transfunction(function, check_decorators=False).get_async_function()
-        if request.param == 'generator':
-            return transfunction(function, check_decorators=False).get_generator_function()
-    return transformator_function
 
 
 @pytest.fixture(params=[Dict, dict])
